5b46a4557a449f50b1361ab91b49efffd93c0f8e
HectorSantiagoEsquivel <hectorsantiagoesquivel@gmail.com>
2024-10-09 21:09:55 +0000

    Punto D completo

---
+++ RTA_SCRIPT_Examen_20241009/Punto_D.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+mkdir -p Estructura_Asimetrica/{correo/{carteros_{1..10},cartas_{1..100}},clientes/cartas_{1..100}}
+
+tree Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
