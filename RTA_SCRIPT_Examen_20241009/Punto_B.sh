9c29009c0b2fbde02cd0b63a702b38b2d5e2c249
HectorSantiagoEsquivel <hectorsantiagoesquivel@gmail.com>
2024-10-10 00:56:35 +0000

    Punto_B funcional

---
+++ RTA_SCRIPT_Examen_20241009/Punto_B.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+DISCO="/dev/sdd"
+
+PESO_TOTAL=$(lsblk -b -d -o SIZE -n $DISCO)
+PESO_TOTAL_CONVERTIDO=$(( PESO_TOTAL / 512 ))
+PESO_PARTICION=$(( PESO_TOTAL_CONVERTIDO / 11 ))
+echo ${PESO_PARTICION}
+
+sudo fdisk "$DISCO" << EOF
+n       
+e       
+
+
+  
+w       
+EOF
+
+
+
+for (( i=5; i<=15; i++ ))
+do
+    
+    sudo fdisk "$DISCO" << EOF
+n
+
++${PESO_PARTICION}
+w
+EOF
+
+    echo "EXITO AL PARTICIONAR $i "
+done
+
+echo
+echo
+
+
+for (( i=5; i<=15; i++ ))
+do
+    PARTICION="${DISCO}${i}"
+    mkfs.ext4 $PARTICION
+
+    echo "EXITO AL FORMATEAR $i"
+done
+
+echo
+echo
+
+CONTADORDISCO=5
+
+for (( i=1; i<=3; i++ )); do
+    
+    for (( j=1; j<=3; j++ )); do
+        
+        echo "${DISCO}${CONTADORDISCO}  /home/Examenes-UTN/alumno_${i}/parcial_${j}   ext4  defaults  0 0" | sudo tee -a /etc/fstab
+        CONTADORDISCO=$((CONTADORDISCO + 1))  
+    done
+done
+echo "${DISCO}${CONTADORDISCO}  /home/Examenes-UTN/profesores  ext4  defaults  0 0" | sudo tee -a /etc/fstab
+
+mount -a
+
